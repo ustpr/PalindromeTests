2b7e27f8014c80bab0c289b43c98f63d7bff51eb
Saigent Zeqja <105798112+ustpr@users.noreply.github.com>
2025-11-09 23:33:01 +0100

    Update PalindromeCheckerTest.java

--- PalindromeCheckerTest.java
+++ PalindromeCheckerTest.java
@@ -2,7 +2,7 @@ package tests;
 
 import main.PalindromeChecker;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 public class PalindromeCheckerTest {
 
